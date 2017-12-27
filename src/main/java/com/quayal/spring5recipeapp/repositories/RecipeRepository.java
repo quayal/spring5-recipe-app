434bdd5d00cadffb495bf3aff8e8b6f28e263f53
Adrian <adrian.grzebyk@gmai.com>
2017-12-27 15:49:42 +0100

    added repositories

---
+++ src/main/java/com/quayal/spring5recipeapp/repositories/RecipeRepository.java
@@ -0,0 +1,7 @@
+package com.quayal.spring5recipeapp.repositories;
+
+import com.quayal.spring5recipeapp.domain.Recipe;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RecipeRepository extends CrudRepository<Recipe, Long>{
+}
